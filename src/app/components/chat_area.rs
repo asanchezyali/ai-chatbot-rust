6f067b37b30d24b88126f130e8d7945cb042f0af
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-04-01 14:05:48 -0500

    ✨ Enhance ChatArea component with improved dark mode support and dynamic message rendering

--- src/app/components/chat_area.rs
+++ src/app/components/chat_area.rs
@@ -1,4 +1,4 @@
-use leptos::{*, html::Div};
+use leptos::{html::Div, *};
 
 use crate::model::conversation::Conversation;
 
@@ -14,15 +14,45 @@ const CHAT_AREA_CLASS: &str = "h-screen pb-24 w-full flex flex-col overflow-y-au
 const CHAT_AREA_LIGHT_MODE_COLORS: &str = "border-gray-300 bg-gray-100";
 const CHAT_AREA_DARK_MODE_COLORS: &str = "border-gray-700 bg-zinc-900";
 
-
 #[component]
 pub fn ChatArea(conversation: ReadSignal<Conversation>) -> impl IntoView {
-    let dark_mode = use_context::<ReadSignal<bool>>().expect("should be able to get dark mode state");
+    let dark_mode =
+        use_context::<ReadSignal<bool>>().expect("should be able to get dark mode state");
 
     let user_message_class = Signa::derive(move || {
         if dark_mode.get() {
             format!("{USER_MESSAGE_CLASS} {USER_MESSAGE_DARK_MODE_COLORS}")
-            
+        } else {
+            format!("{USER_MESSAGE_CLASS} {USER_MESSAGE_LIGHT_MODE_COLORS}")
         }
-    })
+    });
+
+    let chat_div_ref = create_node_ref::<Div>();
+    create_effect(move |_| {
+        conversation.get();
+        if let Some(div) = chat_div_ref.get() {
+            div.set_scroll_top(div.scroll_height());
+        }
+    });
+
+    let chat_area_class = Signal::derive(move || {
+        if dark_mode.get() {
+            format!("{CHAT_AREA_CLASS} {CHAT_AREA_DARK_MODE_COLORS}")
+        } else {
+            format!("{CHAT_AREA_CLASS} {CHAT_AREA_LIGHT_MODE_COLORS}")
+        }
+    });
+
+    view! {
+        <div class={chat_area_class.get()} node_ref=chat_div_ref>
+        {move || conversation.get().messages.iter().map(move |message| {
+            let class_str = if message.user { user_message_class.get() } else { model_message_class.get()};
+            view! {
+                <div class={class_str}>
+                    {message.text.clone()}
+                </div>
+            }
+        }).collect::<Vec<_>>() }
+        </div>
+    }
 }
