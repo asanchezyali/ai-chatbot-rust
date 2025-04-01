8c49549d4cc6a3c87d6e5b0409c368e43db5ff90
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-03-31 22:02:01 -0500

    🧩 Refactor ChatArea component to integrate conversation handling and improve message styling

--- src/app/components/chat_area.rs
+++ src/app/components/chat_area.rs
@@ -1,20 +1,28 @@
-use serde::Deserialize;
-use serde::Serialize;
+use leptos::{*, html::Div};
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct Conversation {
-    pub messages: Vec<Message>,
-}
+use crate::model::conversation::Conversation;
 
-impl Conversation {
-    pub fn new() -> Conversation {
-        Conversation {
-            messages: Vec::new(),
+const USER_MESSAGE_DARK_MODE_COLORS: &str = "bg-blue-500 text-white";
+const USER_MESSAGE_LIGHT_MODE_COLORS: &str = "bg-blue-200 text-black";
+const USER_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-end";
+
+const MODEL_MESSAGE_LIGHT_MODE_COLORS: &str = "bg-gray-200 text-black";
+const MODEL_MESSAGE_DARK_MODE_COLORS: &str = "bg-zinc-700 text-white";
+const MODEL_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-start";
+
+const CHAT_AREA_CLASS: &str = "h-screen pb-24 w-full flex flex-col overflow-y-auto p-5";
+const CHAT_AREA_LIGHT_MODE_COLORS: &str = "border-gray-300 bg-gray-100";
+const CHAT_AREA_DARK_MODE_COLORS: &str = "border-gray-700 bg-zinc-900";
+
+
+#[component]
+pub fn ChatArea(conversation: ReadSignal<Conversation>) -> impl IntoView {
+    let dark_mode = use_context::<ReadSignal<bool>>().expect("should be able to get dark mode state");
+
+    let user_message_class = Signa::derive(move || {
+        if dark_mode.get() {
+            format!("{USER_MESSAGE_CLASS} {USER_MESSAGE_DARK_MODE_COLORS}")
+            
         }
-    }
-}
-
-pub struct Message {
-    pub user: bool,
-    pub text: String,
+    })
 }
